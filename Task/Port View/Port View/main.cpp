898af4bf0fe0d8ba23f5031cfd92f56cc93d6d70
Raisenil <you@example.com>
2021-02-16 08:27:07 +0600

    Scaling & Translate

--- Task/Port View/Port View/main.cpp
+++ Task/Port View/Port View/main.cpp
@@ -91,9 +91,9 @@ void display() {
 	glVertex2f(-0.7f, -0.5f);
 	glVertex2f(-0.7f, -0.8f);
 	glVertex2f(-0.6f, -0.8f);
-	glVertex2f(-0.6f, -0.5f);
-	glVertex2f(-0.4f, -0.4f);
+	glVertex2f(-0.7f, -0.5f);
 	glEnd();
+
 
 	glBegin(GL_LINES);
 	glColor3f(0.0f, 0.0f, 0.0f);
@@ -187,6 +187,66 @@ void display() {
 			    y1 + (radius1 * sin(i * twicePi1 / triangleAmount))
 			);
 		}
+	glEnd();
+
+
+	glTranslatef(-0.8,0.2f,0.0f);
+	//glScalef(2,2,0);
+	glBegin(GL_QUADS);
+	glColor3ub(0, 0, 0);
+	glVertex2f(0.0f, -0.3f);
+	glVertex2f(0.7f, -0.3f);
+
+	glVertex2f(0.8f, -0.2f);
+	glVertex2f(-0.1f, -0.2f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(230, 230, 230);
+	glVertex2f(0.4f, -0.1f);
+	glVertex2f(0.4f, -0.2f);
+
+	glVertex2f(0.7f, -0.2f);
+	glVertex2f(0.7f, -0.1f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(120, 120, 51);
+	glVertex2f(0.0f, -0.2f);
+	glVertex2f(0.3f, -0.2f);
+
+	glVertex2f(0.3f, -0.1f);
+	glVertex2f(0.0f, -0.1f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(102, 102, 102);
+	glVertex2f(-0.8f, -0.8f);
+	glVertex2f(-0.8f, -0.9f);
+
+	glVertex2f(-0.5f, -0.9f);
+	glVertex2f(-0.5f, -0.8f);
+	glEnd();
+
+
+	glTranslatef(+1.4f,-0.07f,0.0f);
+	//glScalef(0.2,0.2,0);
+	glBegin(GL_QUADS);
+	glColor3ub(255, 128, 0);
+	glVertex2f(-0.2f, -0.7f);
+	glVertex2f(-0.2f, -0.9f);
+
+	glVertex2f(0.1f, -0.9f);
+	glVertex2f(0.1f, -0.7f);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glColor3ub(128, 26, 0);
+	glVertex2f(0.1f, -0.7f);
+	glVertex2f(0.1f, -0.9f);
+
+	glVertex2f(0.3f, -0.9f);
+	glVertex2f(0.3f, -0.7f);
 	glEnd();
 
 	glFlush();
